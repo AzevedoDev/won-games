5236f0cba63079546873dbf57411b515da664740
Pedro Azevedo <theazevedo@outlook.com>
2021-04-12 21:54:26 -0300

    feat: add theme on project and fix storybook theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -35,19 +35,17 @@ const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antiliased;
     -moz-osx-font-smoothing: grayscale;
   }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html {
-    font-size: 62.5%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
 `
 
 export default GlobalStyles
